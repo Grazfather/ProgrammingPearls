d0015c3f908772860ef2ace7274ed53216f4891e
Grazfather <Grazfather@gmail.com>
2016-01-06 16:48:25 -0800

    Column 1: Problem 3: Implement bit field sort

--- col1/bitvecs.c
+++ col1/bitvecs.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int bitfield[2];
+#define N 10000000
+
+int bitfield[N / (sizeof(int) << 3)];
 
 int getbit(int index)
 {
@@ -26,12 +28,24 @@ void clrbit(int index)
 
 int main(int argc, char* argv[])
 {
-	bitfield[0] = 0xFFAABB00;
-	bitfield[1] = 0xDEADBABE;
-	// Expected: 00000000110111010101010111111111
-	for(int i = 0; i < (sizeof(bitfield) << 3); i++)
+	int num;
+	// Clear the whole bitfield
+	for(int i = 0; i < N; i++)
+		clrbit(i);
+
+	// For each number that comes in, set that bit
+	while(scanf("%d", &num) != EOF)
 	{
-		printf("%d", getbit(i));
+		setbit(num);
 	}
+
+	// For each bit, if it's set, print it out
+	for(int i = 0; i < N; i++)
+	{
+		if(getbit(i)) {
+			printf("%d\n", i);
+		}
+	}
+
 	return 0;
 }

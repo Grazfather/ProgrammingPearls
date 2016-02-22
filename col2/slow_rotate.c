ce7c720f17221cf6860fea53fbffc40704538807
Grazfather <Grazfather@gmail.com>
2016-02-21 18:08:50 -0800

    Column 2: Problem 3: Solve recursive method

--- col2/slow_rotate.c
+++ col2/slow_rotate.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int gcd(int a, int b)
 {
@@ -38,13 +39,70 @@ void rotate_vec(char *vec, size_t size, size_t rot)
 	}
 }
 
+void swap_vec(char *vec1, char *vec2, int length)
+{
+	for(int i = 0; i < length; i++)
+	{
+		vec1[i] ^= vec2[i];
+		vec2[i] ^= vec1[i];
+		vec1[i] ^= vec2[i];
+	}
+}
+
+void rotate_vec_recursive(char *vec, size_t size, size_t rot)
+{
+	// Recursively swap sections of a vec so that it is in effect rotated to the left rot times.
+	// if we want to rotate the vec rot times, we can break if into two segments, a and b, where a is rot
+	// items long. Now there are four cases for the size of b:
+	// 1. len(b) = 0. No rotation is necessary
+	// 2. len(b) = len(a). Swap and we are done
+	// 3. len(a) > len(b).
+	//   1. Split up a into a_l and a_r, where len(a_r) = len(b)
+	//   2. Swap a_l and b. b is in its final place
+	//   3. Now [a_r, a_l] must be rotated (recurse)
+	// 4. len(a) < len(b).
+	//   1. Split up b into b_l and b_r, where len(b_r) = len(a).
+	//   2. Swap a and b_r. a is in its final place
+	//   3. Now [b_r, b_l] must be rotated (recurse)
+	printf("Rotating %.*s to the left by %d\n", (int)size, vec, (int)rot);
+	rot = rot % size; // Rotating by more than the full length will just loop around
+	if(rot == 0) // Case 1
+	{
+		return;
+	}
+	else if (rot * 2 == size) // Case 2
+	{
+		swap_vec(vec, vec + rot, rot);
+	}
+	else if(rot * 2 > size) // Case 3
+	{
+		// Swap a_l with b
+		swap_vec(vec, vec + rot, size - rot);
+		// Now rotate [a_r, a_l] by 2 * rot - size
+		rotate_vec_recursive(vec + size - rot, rot, 2 * rot - size);
+	} else { // Case 4
+		// Swap a with b_r
+		swap_vec(vec, vec + size - rot, rot);
+		// Now rotate [b_r, b_l] by rot
+		rotate_vec_recursive(vec, size - rot, rot);
+	}
+
+}
+
 int main(int argc, char*argv[])
 {
-	char vec1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', '\0'};
-	size_t n = sizeof(vec1)-1; // -1 to account for the null byte
-	size_t i = 3;
+	char vec[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', '\0'};
+	int n = 4;
+	int l = strlen(vec);
 
-	printf("%s\n", vec1);
-	rotate_vec(vec1, n, i);
-	printf("%s\n", vec1);
+	rotate_vec(vec, l, n);
+	printf("%s\n", vec);
+	rotate_vec(vec, l, l-n);
+	printf("%s\n", vec);
+
+	// Should be back to normal now
+	rotate_vec_recursive(vec, l, n);
+	printf("%s\n", vec);
+	rotate_vec_recursive(vec, l, l-n);
+	printf("%s\n", vec);
 }
